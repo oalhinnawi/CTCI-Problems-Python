bb879bc428f8f0d4f26c6e087970b0af18c34b0d
Omar Alhinnawi <oalhinnawi@gmail.com>
2021-05-28 17:29:55 -0700

    3.1 Psuedo code done

---
+++ three_in_one.py
@@ -0,0 +1,6 @@
+# 3.1 Describe how you could use a single array to implement three stacks.
+
+# First thoughts: Ok this question was really confusing at first, but I understand now.
+# Using one array to implement three stacks, I would split the array into three separate sections.
+# Assuming the array is length some multiple of 3 and is non-zero
+# there'd be some n/3 which determines the sizes of each of the stacks.
